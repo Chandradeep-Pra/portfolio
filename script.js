05d71767bb8d6d9617a281453f930e2309b94dd4
Chandradeep Prasad <69478753+Chandradeep-Pra@users.noreply.github.com>
2023-12-13 23:58:41 +0530

    Update script.js for theme change

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const theme = document.getElementById("theme");
 theme.onclick = function(){
     document.body.classList.toggle("dark-theme")
     if(document.body.classList == "dark-theme"){
-        document.body.style.backgroundImage = "url('assests/background.jpg')";
+        document.body.style.backgroundImage = "url('https://i.ibb.co/74RGfC3/background.jpg')";
+        //<a href="https://ibb.co/8cdjCsy"><img src="https://i.ibb.co/74RGfC3/background.jpg" alt="background" border="0"></a>
         theme.innerHTML="Light"
     }
     else{
     
-    document.body.style.backgroundImage = "url('assests/lightBack.jpg')";
+    document.body.style.backgroundImage = "url('https://i.ibb.co/hHtDx0Z/light-Back.webp')";
          theme.innerHTML="Dark"
     }
 }
